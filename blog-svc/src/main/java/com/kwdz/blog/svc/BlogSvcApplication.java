7a5da464384f23552b24541086886ebee8c523a4
vivtoum <vedyou@qq.com>
2019-04-29 03:25:25 +0800

    添加：单点登录

--- blog-svc/src/main/java/com/kwdz/blog/svc/BlogSvcApplication.java
+++ blog-svc/src/main/java/com/kwdz/blog/svc/BlogSvcApplication.java
@@ -4,6 +4,7 @@ import com.kwdz.blog.svc.common.config.BlogSvcConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
  * BLOG-SVC 启动类
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BlogSvcApplication {
 
     public static void main(String[] args) {
--- blog-svc/src/main/java/com/kwdz/blog/svc/user/facade/UserFacade.java
+++ blog-svc/src/main/java/com/kwdz/blog/svc/user/facade/UserFacade.java
@@ -4,6 +4,7 @@ import com.kwdz.blog.api.user.vo.UserVo;
 import com.kwdz.blog.svc.common.facade.CommonFacade;
 import com.kwdz.blog.svc.user.entity.UserEntity;
 import com.kwdz.blog.svc.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user/")
+@Slf4j
 public class UserFacade extends CommonFacade<UserVo, UserEntity> {
     @Autowired
     private UserService userService;
@@ -25,6 +27,7 @@ public class UserFacade extends CommonFacade<UserVo, UserEntity> {
 
     @GetMapping("getName/{username}")
     UserEntity findByUserName(@PathVariable("username") String userName) {
+        log.info(userService.findByUserName(userName).toString());
         return userService.findByUserName(userName);
     }
 }
