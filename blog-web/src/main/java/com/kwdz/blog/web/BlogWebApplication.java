2bfde40194aaee2774a64f3fc035e3d3390fa964
vivtoum <vedyou@qq.com>
2019-05-21 18:20:31 +0800

    更新：RSA 加密

--- blog-web/src/main/java/com/kwdz/blog/web/BlogWebApplication.java
+++ blog-web/src/main/java/com/kwdz/blog/web/BlogWebApplication.java
@@ -2,6 +2,8 @@ package com.kwdz.blog.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 public class BlogWebApplication {
